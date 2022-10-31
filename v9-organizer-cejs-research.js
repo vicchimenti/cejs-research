74faa82adb3b881e44c7e95cc4e13619f4ba8c3a
Vic Chimenti <victor.chimenti@gmail.com>
2022-10-31 08:59:36 -0700

    rm section id entyr

--- v9-organizer-cejs-research.js
+++ v9-organizer-cejs-research.js
@@ -207,7 +207,7 @@
          let cejsrDict = {
  
              contentName: getContentValues('<t4 type="content" name="Name" output="normal" modifiers="striptags,htmlentities" />'),
-             sectionId: getContentValues('<t4 type="content" name="Section ID" output="normal" modifiers="striptags,htmlentities" />'),
+             
              articleTitle: getContentValues('<t4 type="content" name="Article Title" output="normal" modifiers="striptags,htmlentities" />'),
              courseName: getContentValues('<t4 type="content" name="Course Name" output="normal" modifiers="striptags,htmlentities" />'),
              college: getContentValues('<t4 type="content" name="College" output="normal" modifiers="striptags,htmlentities" />'),
